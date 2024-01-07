b92dd65035cfaacabd81ec8d0259f0b005d07f72
Mohammed Khalid Hamid <khalid64927@gmail.com>
2024-01-07 16:14:57 +0800

    WIP chore

--- shared/src/commonMain/kotlin/com/multiplatform/app/ui/screens/PaymentBottomSheet.kt
+++ shared/src/commonMain/kotlin/com/multiplatform/app/ui/screens/PaymentBottomSheet.kt
@@ -1,11 +1,18 @@
 package com.multiplatform.app.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -16,6 +23,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.multiplatform.app.ui.components.bottomsheet.TransparentBackground
 import kotlinx.coroutines.launch
 
@@ -45,15 +54,33 @@ import kotlinx.coroutines.launch
                         },
                         sheetState = sheetState
                     ) {
-                        Button(onClick = {
-                            scope.launch { sheetState.hide() }.invokeOnCompletion {
-                                if (!sheetState.isVisible) {
-                                    showBottomSheet = false
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp),
+                        ) {
+                            Text(
+                                text = "This is a dummy content inside the bottom sheet",
+                                style = MaterialTheme.typography.headlineMedium
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Text(
+                                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                                style = MaterialTheme.typography.bodyMedium
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Button(onClick = {
+                                scope.launch { sheetState.hide() }.invokeOnCompletion {
+                                    if (!sheetState.isVisible) {
+                                        showBottomSheet = false
+                                    }
                                 }
+                            }) {
+                                Text("Hide bottom sheet")
                             }
-                        }) {
-                            Text("Hide bottom sheet")
+
                         }
+
                     }
                 }
             }
